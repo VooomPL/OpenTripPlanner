15f1fb351c94e67907a60157b59489ceeac141a6
adamwiktor <adam.wiktor97@gmail.com>
2020-08-06 15:02:11 +0200

    Add new edges and vertices classes

---
+++ src/main/java/org/opentripplanner/routing/edgetype/rentedgetype/RentVehicleEdge.java
@@ -0,0 +1,83 @@
+package org.opentripplanner.routing.edgetype.rentedgetype;
+
+import org.opentripplanner.routing.core.State;
+import org.opentripplanner.routing.core.StateEditor;
+import org.opentripplanner.routing.core.vehicle_sharing.VehicleDescription;
+import org.opentripplanner.routing.edgetype.TemporaryEdge;
+import org.opentripplanner.routing.graph.Edge;
+import org.opentripplanner.routing.vertextype.TemporaryRentVehicleVertex;
+
+import java.util.Locale;
+
+/**
+ * This edge allows us to rent vehicle (or leave current vehicle and rent new one).
+ * This edge is a loop on {@link TemporaryRentVehicleVertex} which, when traversed, changes our current traverse mode,
+ * but leaves us in the same location.
+ */
+public class RentVehicleEdge extends Edge implements TemporaryEdge {
+
+    private final VehicleDescription vehicle;
+
+    private final ParkingZoneInfo parkingZones;
+
+    private final ParkingZoneInfo parkingZonesEnabled;
+
+    public RentVehicleEdge(TemporaryRentVehicleVertex v, VehicleDescription vehicle, ParkingZoneInfo parkingZones,
+                           ParkingZoneInfo parkingZonesEnabled) {
+        super(v, v);
+        this.vehicle = vehicle;
+        this.parkingZones = parkingZones;
+        this.parkingZonesEnabled = parkingZonesEnabled;
+    }
+
+    @Override
+    public String getName() {
+        return "Rent vehicle " + vehicle;
+    }
+
+    @Override
+    public String getName(Locale locale) {
+        return "Rent vehicle " + vehicle;
+    }
+
+    @Override
+    public State traverse(State state) {
+        if (!state.getOptions().rentingAllowed) {
+            return null;
+        }
+        if (state.getOptions().vehicleValidator.isValid(vehicle)) {
+            if (state.isCurrentlyRentingVehicle()) {
+                return trySwitchVehicles(state, vehicle);
+            } else {
+                return beginVehicleRenting(state, vehicle);
+            }
+        }
+        return null;
+    }
+
+    // TODO AdamWiktor VMP-4 reverse-optimize
+
+    private State trySwitchVehicles(State state, VehicleDescription vehicle) {
+        if (!canDropoffVehicleHere(state.getCurrentVehicle())) {
+            return null;
+        }
+        StateEditor stateEditor = state.edit(this);
+        stateEditor.doneVehicleRenting();
+        stateEditor.beginVehicleRenting(vehicle);
+        return stateEditor.makeState();
+    }
+
+    private boolean canDropoffVehicleHere(VehicleDescription vehicle) {
+        return !parkingZonesEnabled.appliesToVehicle(vehicle) || parkingZones.appliesToVehicle(vehicle);
+    }
+
+    private State beginVehicleRenting(State state, VehicleDescription vehicle) {
+        StateEditor next = state.edit(this);
+        next.beginVehicleRenting(vehicle);
+        return next.makeState();
+    }
+
+    public VehicleDescription getVehicle() {
+        return vehicle;
+    }
+}
