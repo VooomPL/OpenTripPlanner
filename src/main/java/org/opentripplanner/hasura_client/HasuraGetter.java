83dd0c742805f00d8eb664348f3a0f7d1b13d7fe
mironszewczyk <m.szewczyk@vooom.pl>
2020-09-21 10:24:05 +0200

    When fetching vehicles/bike stations from hasura, use coordinates based only on osm vertices (instead of all vertices)

--- src/main/java/org/opentripplanner/hasura_client/HasuraGetter.java
+++ src/main/java/org/opentripplanner/hasura_client/HasuraGetter.java
@@ -1,13 +1,18 @@
 package org.opentripplanner.hasura_client;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.locationtech.jts.geom.Coordinate;
 import org.opentripplanner.hasura_client.hasura_objects.HasuraObject;
 import org.opentripplanner.hasura_client.mappers.HasuraToOTPMapper;
 import org.opentripplanner.routing.graph.Graph;
+import org.opentripplanner.routing.graph.Vertex;
+import org.opentripplanner.routing.vertextype.OsmVertex;
 import org.opentripplanner.util.HttpUtils;
 import org.slf4j.Logger;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 
@@ -25,10 +30,16 @@ public abstract class HasuraGetter<GRAPH_OBJECT, HASURA_OBJECT extends HasuraObj
     }
 
     private String getGeolocationArguments(Graph graph) {
-        double latMin = graph.getEnvelope().getLowerLeftLatitude();
-        double lonMin = graph.getEnvelope().getLowerLeftLongitude();
-        double latMax = graph.getEnvelope().getUpperRightLatitude();
-        double lonMax = graph.getEnvelope().getUpperRightLongitude();
+        List<OsmVertex> vertices = graph.getVertices().stream()
+                .filter(v -> v instanceof OsmVertex)
+                .map(v -> (OsmVertex) v)
+                .collect(Collectors.toList());
+
+        double latMin = vertices.stream().map(Vertex::getCoordinate).map(Coordinate::getY).min(Comparator.naturalOrder()).get();
+        double lonMin = vertices.stream().map(Vertex::getCoordinate).map(Coordinate::getX).min(Comparator.naturalOrder()).get();
+        double latMax = vertices.stream().map(Vertex::getCoordinate).map(Coordinate::getY).max(Comparator.naturalOrder()).get();
+        double lonMax = vertices.stream().map(Vertex::getCoordinate).map(Coordinate::getX).max(Comparator.naturalOrder()).get();
+
         return "\"variables\": {" +
                 "  \"latMin\": " + latMin + "," +
                 "  \"lonMin\": " + lonMin + "," +
