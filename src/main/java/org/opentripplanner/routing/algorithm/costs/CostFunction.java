8e7d4409d7ef3a5808153f2647b96fa76c1b74b1
paulinaadamska <p.adamska@vooom.pl>
2020-10-07 13:16:56 +0200

    Cost function draft implemented

--- src/main/java/org/opentripplanner/routing/algorithm/costs/CostFunction.java
+++ src/main/java/org/opentripplanner/routing/algorithm/costs/CostFunction.java
@@ -2,7 +2,7 @@ package org.opentripplanner.routing.algorithm.costs;
 
 public interface CostFunction {
 
-    enum CostCategory {ORIGINAL}
+    enum CostCategory {ORIGINAL, PRICE_ASSOCIATED}
 
     double getCostWeight(CostCategory category);
 
