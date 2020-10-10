6427501957aa526ae6832004c716f89d00d2e5e1
mironszewczyk <m.szewczyk@vooom.pl>
2020-10-10 11:04:50 +0200

    Moved calculating to estimatedRemainingWeight to BenchmarkDominanceResource

--- src/main/java/org/opentripplanner/api/resource/BenchmarkDominanceResource.java
+++ src/main/java/org/opentripplanner/api/resource/BenchmarkDominanceResource.java
@@ -62,7 +62,9 @@ public class BenchmarkDominanceResource extends RoutingResource {
 
 
                     Double realW = paths.get(0).getWeight();
-                    Double estimated = paths.get(0).states.get(1).estimatedRemainingWeight;
+                    Double estimated = request.remainingWeightWeight *
+                            request.getRoutingContext().remainingWeightHeuristic.estimateRemainingWeight(paths.get(0).states.get(0));
+
                     Long calculatedTime = paths.get(0).getEndTime() - paths.get(0).getStartTime();
 
                     realRW.add(realW);
