8b99acdcc4644d9bd8a932dfe1fb521e05712922
Kamyk <k.mykitiuk@gmail.com>
2020-12-17 18:00:15 +0100

    Remove vehiclePresence api from health check

--- src/main/java/org/opentripplanner/api/model/RouterHealth.java
+++ src/main/java/org/opentripplanner/api/model/RouterHealth.java
@@ -7,18 +7,14 @@ public class RouterHealth {
 
     private boolean configReady;
     private boolean vehiclePosition;
+    // not vial for health
     private boolean traffic;
+    // not vial for health
     private boolean vehiclePresence;
 
     boolean calculateHealth() {
         boolean health = configReady;
         if (System.getProperty("sharedVehiclesApi") != null) {
-            health &= vehiclePosition;
-        }
-        if (System.getProperty("trfficApi") != null) {
-            health &= traffic;
-        }
-        if (System.getProperty("predictionApiUrl") != null) {
             health &= vehiclePosition;
         }
         return health;
