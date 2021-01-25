d930a44dec445b56527b95ed2cab16d82c77358a
paulinaadamska <p.adamska@vooom.pl>
2021-01-25 10:20:59 +0100

    Implement translating fares number to max travel time without fare switching constraints

--- src/main/java/org/opentripplanner/pricing/TransitTicketType.java
+++ src/main/java/org/opentripplanner/pricing/TransitTicketType.java
@@ -2,15 +2,20 @@ package org.opentripplanner.pricing;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.apache.commons.lang3.tuple.Pair;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.pricing.ticket.pattern.RoutePattern;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class TransitTicketType {
+
+    private enum ConstraintCategory {TIME, ROUTE_PATTERN, MAX_FARES}
 
     @Getter
     private final int id;
@@ -25,29 +30,38 @@ public class TransitTicketType {
     @Getter
     private final RoutePattern routePattern;
 
+    @Getter
+    @Setter
+    private int maxFares = -1;
+
     public int getTotalMinutesWhenValid(int finishesAtMinute, List<Route> sortedRoutes, List<Pair<Integer, Integer>> routeTimeSpans) {
-        int totalMinutesWhenValid = finishesAtMinute;
+        HashMap<ConstraintCategory, Integer> totalMinutesWhenValid = new HashMap<>();
+        totalMinutesWhenValid.put(ConstraintCategory.TIME, maxMinutes > -1 ? maxMinutes : finishesAtMinute);
+        totalMinutesWhenValid.put(ConstraintCategory.ROUTE_PATTERN, finishesAtMinute);
+        totalMinutesWhenValid.put(ConstraintCategory.MAX_FARES, finishesAtMinute);
         //TODO: include stopPattern compliant max time
         //TODO: include maxDistance compliant max time
-        //TODO: include maxFares compliant max time
+        //TODO: rozbić na osobne metody sprawdzania warunków?
 
         //TODO: sortowanie!!
-        if (routePattern != null) {
-            Route currentRoute;
-            for (int i = sortedRoutes.size() - 1; i >= 0; i--) {
-                currentRoute = sortedRoutes.get(i);
-                int currentRouteStartMinute = routeTimeSpans.get(i).getLeft();
-                if (!(currentRouteStartMinute > finishesAtMinute)) {
-                    if (routePattern.matches(currentRoute)) {
-                        totalMinutesWhenValid = finishesAtMinute - currentRouteStartMinute + 1;
-                    } else {
-                        break;
-                    }
+        Route currentRoute;
+        int totalFareCount = 0;
+        for (int i = sortedRoutes.size() - 1; i >= 0; i--) {
+            currentRoute = sortedRoutes.get(i);
+            int currentRouteStartMinute = routeTimeSpans.get(i).getLeft();
+            if (!(currentRouteStartMinute > finishesAtMinute)) {
+                if (totalFareCount < maxFares) {
+                    totalFareCount++;
+                    totalMinutesWhenValid.put(ConstraintCategory.MAX_FARES, finishesAtMinute - currentRouteStartMinute + 1);
+                    //TODO: include fare switching rules
+                }
+                if (routePattern != null && routePattern.matches(currentRoute)) {
+                    totalMinutesWhenValid.put(ConstraintCategory.ROUTE_PATTERN, finishesAtMinute - currentRouteStartMinute + 1);
                 }
             }
         }
 
-        return totalMinutesWhenValid; //TODO: return the result of this computations or maxMinutes/maxDistance-associated time (if some limit = -1, return the length of entire trip length
+        return Collections.min(totalMinutesWhenValid.values());
     }
 
 }
--- src/test/java/org/opentripplanner/pricing/ticket/pattern/TransitTicketTypeTest.java
+++ src/test/java/org/opentripplanner/pricing/ticket/pattern/TransitTicketTypeTest.java
@@ -110,4 +110,196 @@ public class TransitTicketTypeTest {
         assertEquals(45, ticketWithNullRouteConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
     }
 
+    @Test
+    public void shouldReturn15MinutesValidDueToTimeRestrictions() {
+        Route firstRoute = new Route();
+        firstRoute.setId(new FeedScopedId());
+        firstRoute.setShortName("N30");
+        Route secondRoute = new Route();
+        secondRoute.setId(new FeedScopedId());
+        secondRoute.setShortName("13");
+
+        List<Route> sortedRoutes = new ArrayList<>();
+        List<Pair<Integer, Integer>> routeTimeSpans = new ArrayList<>();
+
+        /*
+
+        Routes used in the tested itinerary:
+
+           10 min   5 min           35 min
+        |----------|-----|---------------------------|
+           N30      walk             13
+
+         */
+        sortedRoutes.add(firstRoute);
+        routeTimeSpans.add(new ImmutablePair<>(1, 10));
+        sortedRoutes.add(secondRoute);
+        routeTimeSpans.add(new ImmutablePair<>(16, 50));
+
+        TransitTicketType ticketWithTimeConstraints = new TransitTicketType(4, 15, BigDecimal.valueOf(15), new RoutePattern());
+
+        assertEquals(15, ticketWithTimeConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
+    }
+
+    @Test
+    public void shouldReturn15MinutesValidRegardlessRouteConstraints() {
+        Route firstRoute = new Route();
+        firstRoute.setId(new FeedScopedId());
+        firstRoute.setShortName("N30");
+        Route secondRoute = new Route();
+        secondRoute.setId(new FeedScopedId());
+        secondRoute.setShortName("13");
+
+        List<Route> sortedRoutes = new ArrayList<>();
+        List<Pair<Integer, Integer>> routeTimeSpans = new ArrayList<>();
+
+        /*
+
+        Routes used in the tested itinerary:
+
+           10 min   5 min           35 min
+        |----------|-----|---------------------------|
+           N30      walk             13
+
+         */
+        sortedRoutes.add(firstRoute);
+        routeTimeSpans.add(new ImmutablePair<>(1, 10));
+        sortedRoutes.add(secondRoute);
+        routeTimeSpans.add(new ImmutablePair<>(16, 50));
+
+        TransitTicketType ticketWithTimeAndRouteConstraints = new TransitTicketType(4, 15, BigDecimal.valueOf(15), new RoutePattern());
+        ticketWithTimeAndRouteConstraints.getRoutePattern().addConstraint(RoutePattern.RouteAttribute.SHORT_NAME, Pattern.TextOperator.NOT_STARTS_WITH, "N");
+
+        assertEquals(15, ticketWithTimeAndRouteConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
+    }
+
+    @Test
+    public void shouldReturn45MinutesValidDueToMaxFaresLimitOnly() {
+        Route firstRoute = new Route();
+        firstRoute.setId(new FeedScopedId());
+        firstRoute.setShortName("105");
+        Route secondRoute = new Route();
+        secondRoute.setId(new FeedScopedId());
+        secondRoute.setShortName("13");
+
+        List<Route> sortedRoutes = new ArrayList<>();
+        List<Pair<Integer, Integer>> routeTimeSpans = new ArrayList<>();
+
+        /*
+
+        Routes used in the tested itinerary:
+
+           10 min   5 min           35 min
+        |----------|-----|---------------------------|
+           105      walk             13
+
+         */
+        sortedRoutes.add(firstRoute);
+        routeTimeSpans.add(new ImmutablePair<>(1, 10));
+        sortedRoutes.add(secondRoute);
+        routeTimeSpans.add(new ImmutablePair<>(16, 50));
+
+        TransitTicketType ticketWithMaxFaresConstraints = new TransitTicketType(4, -1, BigDecimal.valueOf(15), null);
+        ticketWithMaxFaresConstraints.setMaxFares(2);
+
+        assertEquals(45, ticketWithMaxFaresConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
+    }
+
+    @Test
+    public void shouldReturn30MinutesValidDueToMaxFaresLimitOnly() {
+        Route firstRoute = new Route();
+        firstRoute.setId(new FeedScopedId());
+        firstRoute.setShortName("105");
+        Route secondRoute = new Route();
+        secondRoute.setId(new FeedScopedId());
+        secondRoute.setShortName("13");
+
+        List<Route> sortedRoutes = new ArrayList<>();
+        List<Pair<Integer, Integer>> routeTimeSpans = new ArrayList<>();
+
+        /*
+
+        Routes used in the tested itinerary:
+
+           10 min   5 min           35 min
+        |----------|-----|---------------------------|
+           105      walk             13
+
+         */
+        sortedRoutes.add(firstRoute);
+        routeTimeSpans.add(new ImmutablePair<>(1, 10));
+        sortedRoutes.add(secondRoute);
+        routeTimeSpans.add(new ImmutablePair<>(16, 50));
+
+        TransitTicketType ticketWithMaxFaresConstraints = new TransitTicketType(4, -1, BigDecimal.valueOf(15), null);
+        ticketWithMaxFaresConstraints.setMaxFares(1);
+
+        assertEquals(30, ticketWithMaxFaresConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
+    }
+
+    @Test
+    public void shouldReturn30MinutesValidDueToMaxFaresLimit() {
+        Route firstRoute = new Route();
+        firstRoute.setId(new FeedScopedId());
+        firstRoute.setShortName("105");
+        Route secondRoute = new Route();
+        secondRoute.setId(new FeedScopedId());
+        secondRoute.setShortName("13");
+
+        List<Route> sortedRoutes = new ArrayList<>();
+        List<Pair<Integer, Integer>> routeTimeSpans = new ArrayList<>();
+
+        /*
+
+        Routes used in the tested itinerary:
+
+           10 min   5 min           35 min
+        |----------|-----|---------------------------|
+           105      walk             13
+
+         */
+        sortedRoutes.add(firstRoute);
+        routeTimeSpans.add(new ImmutablePair<>(1, 10));
+        sortedRoutes.add(secondRoute);
+        routeTimeSpans.add(new ImmutablePair<>(16, 50));
+
+        TransitTicketType ticketWithRouteAndMaxFaresConstraints = new TransitTicketType(4, -1, BigDecimal.valueOf(15), new RoutePattern());
+        ticketWithRouteAndMaxFaresConstraints.setMaxFares(1);
+        ticketWithRouteAndMaxFaresConstraints.getRoutePattern().addConstraint(RoutePattern.RouteAttribute.SHORT_NAME, Pattern.TextOperator.NOT_STARTS_WITH, "N");
+
+        assertEquals(30, ticketWithRouteAndMaxFaresConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
+    }
+
+    @Test
+    public void shouldReturn30MinutesValidDueToRouteConstraints() {
+        Route firstRoute = new Route();
+        firstRoute.setId(new FeedScopedId());
+        firstRoute.setShortName("N30");
+        Route secondRoute = new Route();
+        secondRoute.setId(new FeedScopedId());
+        secondRoute.setShortName("13");
+
+        List<Route> sortedRoutes = new ArrayList<>();
+        List<Pair<Integer, Integer>> routeTimeSpans = new ArrayList<>();
+
+        /*
+
+        Routes used in the tested itinerary:
+
+           10 min   5 min           35 min
+        |----------|-----|---------------------------|
+           N30      walk             13
+
+         */
+        sortedRoutes.add(firstRoute);
+        routeTimeSpans.add(new ImmutablePair<>(1, 10));
+        sortedRoutes.add(secondRoute);
+        routeTimeSpans.add(new ImmutablePair<>(16, 50));
+
+        TransitTicketType ticketWithRouteAndMaxFaresConstraints = new TransitTicketType(4, -1, BigDecimal.valueOf(15), new RoutePattern());
+        ticketWithRouteAndMaxFaresConstraints.getRoutePattern().addConstraint(RoutePattern.RouteAttribute.SHORT_NAME, Pattern.TextOperator.NOT_STARTS_WITH, "N");
+        ticketWithRouteAndMaxFaresConstraints.setMaxFares(2);
+        assertEquals(30, ticketWithRouteAndMaxFaresConstraints.getTotalMinutesWhenValid(45, sortedRoutes, routeTimeSpans));
+    }
+
 }
