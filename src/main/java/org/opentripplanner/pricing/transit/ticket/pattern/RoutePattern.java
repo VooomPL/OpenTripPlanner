6092c1c0fd977ea5f5a37dc777de7be0e15c7c75
paulinaadamska <p.adamska@vooom.pl>
2021-02-10 11:18:48 +0100

    Add agency to route pattern

--- src/main/java/org/opentripplanner/pricing/transit/ticket/pattern/RoutePattern.java
+++ src/main/java/org/opentripplanner/pricing/transit/ticket/pattern/RoutePattern.java
@@ -4,15 +4,25 @@ import org.opentripplanner.model.Route;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class RoutePattern extends Pattern<Route> {
 
-    public enum RouteAttribute {ID, SHORT_NAME, LONG_NAME, TYPE}
+    public enum RouteAttribute {ID, SHORT_NAME, LONG_NAME, TYPE, AGENCY_NAME}
 
     private final HashMap<Pattern.TextOperator, ArrayList<String>> idConstraints = new HashMap<>();
     private final HashMap<Pattern.TextOperator, ArrayList<String>> shortNameConstraints = new HashMap<>();
     private final HashMap<Pattern.TextOperator, ArrayList<String>> longNameConstraints = new HashMap<>();
     private final HashMap<Pattern.NumericalOperator, Double> typeConstraints = new HashMap<>();
+    private final String agencyNameConstraint;
+
+    public RoutePattern() {
+        this(null);
+    }
+
+    public RoutePattern(String agencyName) {
+        this.agencyNameConstraint = agencyName;
+    }
 
     public void addConstraint(RouteAttribute attribute, Pattern.TextOperator operator, String patternValue) {
         switch (attribute) {
@@ -45,7 +55,8 @@ public class RoutePattern extends Pattern<Route> {
         return Pattern.matches(idConstraints, validatedObject.getId().getId()) &&
                 Pattern.matches(shortNameConstraints, validatedObject.getShortName()) &&
                 Pattern.matches(longNameConstraints, validatedObject.getLongName()) &&
-                Pattern.matches(typeConstraints, Double.valueOf(validatedObject.getType()));
+                Pattern.matches(typeConstraints, Double.valueOf(validatedObject.getType())) &&
+                (Objects.isNull(agencyNameConstraint) || agencyNameConstraint.equals(validatedObject.getAgency().getName()));
     }
 
 }
