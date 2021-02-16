77887c237a267fd40b48a725c1707aa34d17d871
paulinaadamska <p.adamska@vooom.pl>
2021-02-16 12:01:24 +0100

    Add transit trip stage representation

---
+++ src/main/java/org/opentripplanner/pricing/transit/trip/model/TransitTripStage.java
@@ -0,0 +1,30 @@
+package org.opentripplanner.pricing.transit.trip.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.opentripplanner.model.Route;
+import org.opentripplanner.model.Stop;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class TransitTripStage {
+
+    @Getter
+    private final Route currentRoute;
+
+    @Getter
+    private final Stop currentStop;
+
+    @Getter
+    private final int time;
+
+    @Getter
+    private final int distance;
+
+    public String toString() {
+        return Optional.of("{" + currentRoute.getShortName()).orElseGet(currentRoute::getLongName) + ", "
+                + currentStop.getName() + ", " + time + " min, " + distance + " m}";
+
+    }
+}
