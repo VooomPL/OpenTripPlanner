55aee7b4d406744ff1323bc7fa6bfa74ea70e079
paulinaadamska <p.adamska@vooom.pl>
2021-02-26 10:23:06 +0100

    Minor refactor of TransitTripStage

--- src/main/java/org/opentripplanner/pricing/transit/trip/model/TransitTripStage.java
+++ src/main/java/org/opentripplanner/pricing/transit/trip/model/TransitTripStage.java
@@ -1,29 +1,25 @@
 package org.opentripplanner.pricing.transit.trip.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Value;
 import org.opentripplanner.model.Route;
 import org.opentripplanner.model.Stop;
 
 import java.util.Optional;
 
-@AllArgsConstructor
+@Value
 public class TransitTripStage {
 
-    @Getter
-    private final Route currentRoute;
+    Route currentRoute;
 
-    @Getter
-    private final Stop currentStop;
+    //Stop at which this stage begins
+    Stop currentStop;
 
-    @Getter
-    private final int time;
+    int time;
 
-    @Getter
-    private final double distance;
+    double distance;
 
     public String toString() {
-        return Optional.of("{" + currentRoute.getShortName()).orElseGet(currentRoute::getLongName) + ", "
+        return "{" + Optional.of(currentRoute.getShortName()).orElseGet(currentRoute::getLongName) + ", "
                 + currentStop.getName() + ", " + time + " min, " + distance + " m}";
 
     }
