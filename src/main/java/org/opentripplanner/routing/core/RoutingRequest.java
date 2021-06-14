06b423981c701aaec641cbf4ddcb94d97ea66430
mironszewczyk <m.szewczyk@vooom.pl>
2021-06-14 13:23:19 +0200

    VMP-238 Allow timeouts a request parameter

--- src/main/java/org/opentripplanner/routing/core/RoutingRequest.java
+++ src/main/java/org/opentripplanner/routing/core/RoutingRequest.java
@@ -237,7 +237,7 @@ public class RoutingRequest implements Cloneable, Serializable {
      */
     public RoutingRequest bikeWalkingOptions;
 
-    public double[] timeouts;
+    public double[] timeouts = {5D, 2D, 1D};
     /**
      * This is true when a GraphPath is being traversed in reverse for optimization purposes.
      */
