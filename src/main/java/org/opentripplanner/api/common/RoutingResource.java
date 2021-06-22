3c0aed0e51a4437c9a7e616d8de5d9ee20690e88
paulinaadamska <p.adamska@vooom.pl>
2021-06-22 16:02:36 +0200

    Add checking whether snapshot is supported according to configuration

--- src/main/java/org/opentripplanner/api/common/RoutingResource.java
+++ src/main/java/org/opentripplanner/api/common/RoutingResource.java
@@ -899,7 +899,8 @@ public abstract class RoutingResource {
             try {
                 LocalDateTime timestamp = LocalDateTime.parse(snapshotTimestamp);
                 SharedVehiclesSnapshotLabel requestedSnapshotLabel = new SharedVehiclesSnapshotLabel(timestamp);
-                if (router.graph.getSupportedSnapshotLabels().get(requestedSnapshotLabel) > 0) {
+                Long numberOfVehiclesInSnapshot = router.graph.getSupportedSnapshotLabels().get(requestedSnapshotLabel);
+                if (Objects.nonNull(numberOfVehiclesInSnapshot) && numberOfVehiclesInSnapshot > 0) {
                     request.setAcceptedSharedVehiclesSnapshotLabel(requestedSnapshotLabel);
                 } else {
                     throw new RuntimeException("Requested snapshot timestamp is currently not supported or available vehicles list is empty");
